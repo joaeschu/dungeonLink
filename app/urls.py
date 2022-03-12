99da639c80ff570d52d130175a44fd2f319b6a65
Joan <joaeschu@inf.upv.es>
2022-03-12 15:12:16 +0100

    first commit

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('private-area', views.private_area, name='private-area'),
+    path('logout', views.logout, name='logout'),
+    path('sheet', views.sheet, name='sheet'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -3,5 +3,52 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def private_area(request):
+    if request.session.get('user_id') is None:
+        return redirect('index')
+    return render(request, 'private_area.html', {'session': request.session})
+
+
+def sheet(request):
+    if request.session.get('user_id') is None:
+        return redirect('index')
+    file = 'json/sheet' + str(request.session['user_id']) + '.json'
+    sheet = None
+    if request.method == 'POST':
+        sheet = request.POST
+        if (os.path.isfile(BASE_DIR / file)):
+            os.remove(BASE_DIR / file)
+        f = open(BASE_DIR / file, "a")
+        f.write(json.dumps(sheet))
+        f.close()
+        return redirect('sheet')
+
+    if (os.path.isfile(BASE_DIR / file)):
+        sheet = json.load(open(BASE_DIR / file, "r"))
+    return render(request, 'sheet.html', {'session': request.session, 'sheet': sheet})
+
+
 def index(request):
-    return render(request, 'login.html', {'name': request.session['name']})
+    context = {'error': None}
+    if request.method == 'POST':
+        users = json.load(open(BASE_DIR / 'json/users.json', "r"))
+
+        email = request.POST['email']
+        password = request.POST['password']
+        actual_user = users.get(email)
+
+        if actual_user is not None and password == actual_user['password']:
+            request.session['user_id'] = actual_user['id']
+            request.session['name'] = actual_user['name']
+            context['session'] = request.session
+            return redirect('private-area')
+        else:
+            context['error'] = 'Usuario o contraseña invalidos'
+
+    return render(request, 'login.html', context)
+
+
+def logout(request):
+    del request.session['user_id']
+    del request.session['name']
+    return redirect('index')
