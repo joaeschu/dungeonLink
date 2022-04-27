629a194bf52f0ebe8aaba802976a5b5f7dd7b66e
Joan <joaeschu@inf.upv.es>
2022-04-27 19:15:40 +0200

    Entrada de datos de clase

--- ficheroClases.py
+++ ficheroClases.py
@@ -36,7 +36,74 @@ clases = {
                   "titución. Puedes usar un escudo y aun así beneficiarte de este rasgo."
                     }
                 },
-        "Bardo": {"Colegio del Conocimiento", "Colegio del Valor"},
+        "Bardo":  {
+            "Dado de golpe": 8,
+            "Armaduras": ["Armaduras ligeras"],
+            "Armas": ["Armas c/c simples", "Armas a distancia simples", "ballesta de mano", "espada larga", "estoque",
+                      "espada corta"],
+            "Herramientas": ["tres instrumentos musicales de tu elección"],
+            "Salvaciones": ["Destreza,", "Carisma."],
+            "Habilidades a elegir": 3,
+            "Habilidades": ["Atletismo", "Acrobacias", "Juego de Manos", "Sigilo", "Arcano", "Historia",
+                     "Investigación", "Naturaleza", "Religión", "Trato de animales", "Medicina", "Percepción", "Perspicacia", "Supervivencia",
+                     "Engaño", "Intimidación", "Interpretación", "Persuasión"],
+            "Subclases": ["Colegio del Conocimiento", "Colegio del Valor"],
+            "Equipo A": ["Estoque", "Equipo de diplomático","lira"],
+            "Equipo B": ["Espada larga", "Equipo de actor", "cualquier otro instrumento musical"],
+            "Equipo obligatorio": ["Armadura de cuero", "Daga"],
+            "Nivel 1": {
+                "Lanzamiento de conjuros":
+                    "Has aprendido a descifrar y reformular la realidad en armonía con tu música y tus deseos. Tus conjuros son parte de tu vasto repertorio, magia que puedes aplicar a variadas situaciones. Consulta el Capítulo 10 para ver las reglas generales de lanzamiento de conjuros y el Capítulo 11 para ver la lista de conjuros del bardo."
+                    "TRUCOS"
+                    "Conoces dos trucos de tu elección de la lista de conjuros del bardo. Aprendes trucos de bardo adicionales en niveles supe-riores, como se ve en la columna Trucos Conocidos de la tabla Bardo."
+                    "ESPACIOS DE CONJURO"
+                    "La tabla Bardo muestra cuántos espacios de conjuro tienes para lanzar tus conjuros de nivel 1 y superior. Para lanzar uno de estos conjuros, debes gastar un espacio de nivel de conjuro o de un nivel superior. Recuperas todos los espacios de conjuro usados cuando terminas un descanso prolongado."
+                    "Por ejemplo, si conoces el conjuro de nivel 1, curar heridas y tienes disponibles un espacio de nivel 1 y otro de nivel 2, puedes lanzar curar heridas usando cualquiera de los dos espacios."
+                    "CONJUROS CONOCIDOS DE NIVEL 1 Y SUPERIOR"
+                    "Conoces cuatro conjuros de nivel 1 de tu elección de la lista de conjuros del bardo. La columna Conjuros Conocidos de la tabla Bardo te muestra cuándo aprendes nuevos conjuros de tu elección. Cada uno de estos hechizos debe ser de un nivel para el cual tengas espacios de conjuro, tal como se ve en la tabla. Por ejemplo, cuando alcanzas el nivel 3, puedes aprender un conjuro de nivel 1 o 2."
+                    "Adicionalmente, cuando ganas un nivel en esta clase, puedes elegir uno de los conjuros de bardo que conoces y reemplazarlo por otro de la lista de conjuros del bardo, que también debe ser de un nivel para el que tengas espacios de conjuros.",
+                    "CARACTERÍSTICA PARA EL LANZAMIENTO DE CONJUROS"
+                    "Carisma es tu característica de lanzamiento para tus conjuros"
+                    "de bardo. Tu magia viene del corazón y el alma que pones en"
+                    "la interpretación de tu música u oración. Usas tu Carisma"
+                    "siempre que un conjuro se refiera a tu habilidad de lanzamiento de conjuros. "
+                    "Además, usas tu modificador de Carisma"
+                    "para determinar la CD de la salvación de un conjuro de bardo"
+                    "que lances y cuando haces una tirada de ataque con uno de"
+                    "tus conjuros de bardo."
+                    "CD de la Salvación de un Conjuro = 8 + tu bonificador de"
+                    "competencia + tu modificador de Carisma"
+                    "Modificador de Ataque de un Conjuro = tu bonificador de"
+                    "competencia + tu modificador de Carisma"
+                    "CONJURACIÓN RITUAL"
+                    "Puedes lanzar cualquier conjuro de bardo que conozcas"
+                    "como un ritual si ese conjuro tiene el descriptor ritual."
+                    "FOCO ARCANO"
+                    "Puedes usar un instrumento musical (listados en el Capítulo"
+                    "5) como foco arcano para tus conjuros de bardo."
+                "inspiración de bardo":
+                    "Puedes inspirar a otros a través de palabras estimulantes o"
+                    "de la música. Para hacerlo, usas una acción adicional en tu"
+                    "turno para elegir una criatura distinta de ti mismo dentro de"
+                    "un rango de 60 pies (18 metros) y que pueda escucharte. Esa"
+                    "criatura gana un dado de Inspiración de Bardo, un d6."
+                    "Una vez, dentro de los siguientes 10 minutos, esa criatura"
+                    "puede tirar el dado y agregar el resultado a una prueba de"
+                    "habilidad, una tirada de ataque o de salvación que haga. La"
+                    "criatura puede esperar a ver el resultado del d20 antes de de-"
+                    "cidir usar el dado de Inspiración de Bardo, pero debe hacerlo"
+                    "antes de que el DM diga si la tirada tuvo éxito o no. Una vez"
+                    "que el dado de Inspiración de Bardo es usado, se pierde. Una"
+                    "criatura sólo puede tener un dado de Inspiración de Bardo a"
+                    "la vez."
+                    "Puedes usar este rasgo una cantidad de veces equivalente"
+                    "a tu modificador de Carisma (mínimo 1). Recuperas todos los"
+                    "usos gastados cuando terminas un descanso prolongado."
+                    "Tu dado de Inspiración de Bardo cambia cuando alcanzas"
+                    "determinados niveles. El dado se convierte en 1d8 al nivel 5,"
+                    "en 1d10 al nivel 10 y en 1d12 al nivel 15."
+                        }
+                    },
         "Brujo": {"el Archihada", "el Diablo", "el Gran Antiguo"},
         "Clérigo": {"Conocimiento", "Vida", "Luz", "Naturaleza", "Tempestad", "Superchería", "Guerra"},
         "Druida": {"Círculo de la Tierra", "el Círculo de la Luna"},
